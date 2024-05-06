f25898a3dd15ab733acb628d6045764e51c921b9
Aniket Mane <111334212+aniketmane18@users.noreply.github.com>
2024-05-06 19:33:44 +0530

    Add files via upload

---
+++ Input.java
@@ -0,0 +1,9 @@
+import java.util.* ;
+public class Input {
+    public static void main(String args[]){
+        Scanner Sc = new Scanner(System.in);
+        String name = Sc.nextLine();
+        System.out.println(name);
+    }
+    
+}
